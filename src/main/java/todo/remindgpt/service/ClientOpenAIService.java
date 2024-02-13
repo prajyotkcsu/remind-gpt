f625902b4a40200e05b829f2df5169981693654e
Prajyot Karnik <prajyotkcsu@gmail.com>
2024-02-12 19:02:52 -0600

    defined clear methods and code flow.

--- src/main/java/todo/remindgpt/service/ClientOpenAIService.java
+++ src/main/java/todo/remindgpt/service/ClientOpenAIService.java
@@ -6,10 +6,7 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import todo.remindgpt.model.*;
@@ -35,6 +32,8 @@ public class ClientOpenAIService {
     private String content;
     @Value("${openai.secret}")
     private String secret;
+    @Value("${openai.url}")
+    private String url;
 
     public ClientOpenAIService() {
     }
@@ -48,13 +47,52 @@ public class ClientOpenAIService {
             category.setKey("cat");
             category.setValue(partition++);
             categoryCacheRepository.save(category);
-
         }
         log.info("{} new categories saved", categories.length);
-    return categories.length;
+        return categories.length;
     }
 
-    public String initPrompt(TaskInputPayload tasks){
+    public String initPrompt(TaskInputPayload tasks) {
+        String prompt = generatePrompt(tasks);
+        log.info("prompt sending to openai:{}", prompt);
+        HttpEntity<OpenAIPayload> requestEntity = openAPIRequestBuilder(prompt);
+        boolean retry = false;
+        while (!retry) {
+            //todo: call openai api here and responde with map of task and type
+            ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+            Map<String, String> taskType = getTaskAndType(responseEntity.getBody().toString());
+
+        }
+        return "";
+    }
+
+    Map<String,String> getTaskAndType(String responseJSON){
+        Map<String,String> hm=new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String out = "";
+        try {
+            JsonNode jsonNode = objectMapper.readTree(responseJSON);
+            out = jsonNode.path("choices").get(0).path("message").path("content").asText();
+        } catch (Exception ex) {
+
+        }
+        String[] taskList = out.split("\n");
+        List<String> producedCategories = new ArrayList<>();
+        for (String task : taskList) {
+            String[] pair = task.split(":");
+            hm.put(pair[1].trim().toLowerCase(),pair[0].trim().toLowerCase());
+        }
+
+//        if (!catMap.containsKey(key)) {
+//            log.info("category returned by openAI is not declared by the user");
+//            continue;
+//        } else {
+//            kafkaService.produce(catMap.get(key), key, value);
+//            producedCategories.add(key + " " + value);
+//        }
+        return hm;
+    }
+    String generatePrompt(TaskInputPayload tasks){
         List<String> taskList=new ArrayList<>();
         for(int i=0;i<tasks.getTasks().size();i++){
             taskList.add(tasks.getTasks().get(i).getTaskDesc());
@@ -74,46 +112,19 @@ public class ClientOpenAIService {
 
         content=content.replace("[tasks]",String.join(",",taskArray));
         prompt=content.replace("[categories]",String.join(",",categoriesArray));
-        log.info("prompt sending to openai:{}", prompt);
+        return prompt;
+    }
+    HttpEntity<OpenAIPayload> openAPIRequestBuilder(String prompt){
         HttpHeaders httpHeaders=new HttpHeaders();
         httpHeaders.setBearerAuth(secret);
         OpenAIPayload openAIPayload = new OpenAIPayload();
         openAIPayload.setModel("gpt-3.5-turbo");
-
         Messages message = new Messages();
         message.setRole("user");
         message.setContent(prompt);
         List<Messages> messagesList = Arrays.asList(message);
         openAIPayload.setMessages(messagesList);
         HttpEntity<OpenAIPayload> requestEntity = new HttpEntity<>(openAIPayload, httpHeaders);
-
-        //todo: call openai api here and responde with map of task and type
-        ResponseEntity<String> responseEntity=restTemplate.exchange("https://api.openai.com/v1/chat/completions", HttpMethod.POST,requestEntity, String.class);
-        String jsonString=responseEntity.getBody().toString();
-        ObjectMapper objectMapper=new ObjectMapper();
-        String out="";
-        try {
-            JsonNode jsonNode=objectMapper.readTree(jsonString);
-            out=jsonNode.path("choices").get(0).path("message").path("content").asText();
-        }
-        catch(Exception ex){
-
-        }
-        String[] response=out.split("\n");
-        List<String> producedCategories=new ArrayList<>();
-        for(String task: response){
-            String[] pair=task.split(":");
-            String key=pair[1].trim().toLowerCase();
-            String value=pair[0].trim();
-            if(!catMap.containsKey(key)){
-                log.info("category returned by openAI is not declared by the user");
-                continue;
-            }
-            else {
-                kafkaService.produce(catMap.get(key), key, value);
-                producedCategories.add(key+" "+value);
-            }
-        }
-        return producedCategories.toArray(new String[0]).toString();
+        return requestEntity;
     }
 }
